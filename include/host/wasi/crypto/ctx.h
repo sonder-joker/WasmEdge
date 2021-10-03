eca206803d2439c888ee2ab0b68a733009896d92
sonder-joker <highmagic@outlook.com>
2021-10-03 02:57:16 +0000

    [WASI] Introduce CryptoCtx interface for control wasi-crypto
    Signed-off-by: sonder-joker <highmagic@outlook.com>

---
+++ include/host/wasi/crypto/ctx.h
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
+
+
+namespace WasmEdge {
+namespace Host {
+namespace WASI {
+namespace Crypto {
+
+class CryptoCtx {
+public:
+    CryptoCtx();
+};
+
+} // namespace Crypto
+} // namespace WASI
+} // namespace Host
+} // namespace WasmEdge
---
+++ lib/host/wasi/crypto/ctx.cpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#include "host/wasi/crypto/ctx.h"
+
+namespace WasmEdge {
+namespace Host {
+namespace WASI {
+namespace Crypto {
+CryptoCtx::CryptoCtx() {}
+
+} // namespace Crypto
+} // namespace WASI
+} // namespace Host
+} // namespace WasmEdge
